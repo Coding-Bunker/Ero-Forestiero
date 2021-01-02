f553b9053908e9b9dcd673bc826d9c02a8a9e5d8
andrea-artuso <ultrandr3x@gmail.com>
2021-01-02 13:00:00 +0100

    Developed and styled Intro section

--- front-page.php
+++ front-page.php
@@ -1,3 +1,10 @@
-<?php
+<?php get_header(); ?>
 
-?>
+        <section class="intro clearfix">
+            <h1 class="intro__title">Imparare l'italiano si può ed è anche <br><strong>gratis</strong>!</h1>
+
+            <a href="#" class="intro__button-primary"><strong>Inizia ad imparare</strong></a>
+            <a href="#" class="intro__link">Scropri l'offerta formativa</a>
+
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ero-forestiero-logo.png" alt="ero-forestiero-logo" class="intro__image">
+        </section>
