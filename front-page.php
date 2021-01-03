744fcbed4f66b3881286eecfc80f4e094b77fdf3
andrea-artuso <ultrandr3x@gmail.com>
2021-01-03 10:01:26 +0100

    Re-styled intro section with Flexbox

--- front-page.php
+++ front-page.php
@@ -1,10 +1,16 @@
 <?php get_header(); ?>
 
         <section class="intro clearfix">
+            <span class="left-column">
             <h1 class="intro__title">Imparare l'italiano si può ed è anche <br><strong>gratis</strong>!</h1>
 
-            <a href="#" class="intro__button-primary"><strong>Inizia ad imparare</strong></a>
-            <a href="#" class="intro__link">Scropri l'offerta formativa</a>
+            <div class="intro-links">
+                <a href="#" class="intro__button-primary"><strong>Inizia ad imparare</strong></a>
+                <a href="#" class="intro__link">Scropri l'offerta formativa</a>
+            </div>
+            </span>
 
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ero-forestiero-logo.png" alt="ero-forestiero-logo" class="intro__image">
+            <span class="right-column">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ero-forestiero-logo.png" alt="ero-forestiero-logo" class="intro__image">
+            </span>
         </section>
