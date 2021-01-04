255f56657009030692ed17eb25c3df7b2d777932
Alessandro Di Maria <alessandrodimaria3@gmail.com>
2021-01-04 23:12:10 +0100

    Develop "become a volunteer" section

--- front-page.php
+++ front-page.php
@@ -34,3 +34,32 @@
 
             <a href="#" class="about__button-primary">Scopri di più</a>
         </section>
+        
+        <!-- Inizio sezione "Diventa un volontario" Author: Alex-->
+        <section class="clearfix volunteer">
+            <h2>Diventa un volontario</h2>
+
+            <div class="volunteer-div clearfix" style="">
+                <span class="volunteer-img">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_quiz_nlyh 1.png">
+                </span>    
+            
+                <span class="volunteer-p">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa consectetur,
+                        architecto expedita temporibus illo cum nihil veniam laborum, quasi iusto inventore sunt,
+                        autem et numquam placeat suscipit odit consequatur velit!
+                    </p>
+
+                    <br>
+
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus,
+                        possimus in aliquam natus eveniet unde dolore magni sequi alias dignissimos
+                        repellendus ducimus adipisci beatae neque consequatur voluptatibus consequuntur labore.
+                    </p>
+                </span>
+            </div>
+
+            <a href="#" class="about__button-primary">Diventa un volontario</a>
+        </section>
