2afa0f50ea3b18d5216616445c93258342bc88c4
andrea-artuso <ultrandr3x@gmail.com>
2021-01-05 10:27:29 +0100

    Developed and style Corsi section

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-        <!-- Inizio sezione intro -->
+        <!-- Inizio sezione intro - Author: Andrea -->
         <section class="intro clearfix">
             <span class="intro__left-column">
             <h1 class="intro__title">Imparare l'italiano si può ed è anche <br><strong>gratis</strong>!</h1>
@@ -17,7 +17,7 @@
         </section>
         <!-- Fine sezione intro -->
 
-        <!-- Inizio sezione Chi Siamo -->
+        <!-- Inizio sezione Chi Siamo - Author: Andrea -->
         <section class="who-we-are clearfix">
             <h2 class="about__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
 
@@ -33,4 +33,32 @@
             </div>
 
             <a href="#" class="about__button-primary">Scopri di più</a>
+        </section>
+        <!-- Fine sezione Chi Siamo -->
+
+        <!-- Inizio sezione Corsi - Author: Andrea -->
+        <section class="courses clearfix">
+            <h2 class="courses__title">I nostri corsi</h2>
+
+            <div class="courses__card-container">
+                <div class="courses__card">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons8-seed-96 1.png" alt="seed-icon" class="courses__card-image">
+                    <h3 class="courses__card-title">Base</h3>
+                    <p class="courses__card-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+
+                <div class="courses__card">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons8-garden-96 1.png" alt="seed-icon" class="courses__card-image">
+                    <h3 class="courses__card-title">Intermedio</h3>
+                    <p class="courses__card-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+
+                <div class="courses__card">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons8-tree-planting-96 1.png" alt="seed-icon" class="courses__card-image">
+                    <h3 class="courses__card-title">Avanzato</h3>
+                    <p class="courses__card-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+
+            <a href="#" class="courses__button-primary">Scopri i nostri corsi</a>
         </section>
