c8b35230021c18104d83d2d055bb11e17c561eb3
Alessandro Di Maria <alessandrodimaria3@gmail.com>
2021-01-09 18:28:40 +0100

    Added "contact" section

--- front-page.php
+++ front-page.php
@@ -62,3 +62,29 @@
 
             <a href="#" class="courses__button-primary">Scopri i nostri corsi</a>
         </section>
+
+        <!-- Inizio sezione contatti Author: Alex -->
+        <section class="contact clearfix">
+            <h2>Contatti</h2>
+
+            <div class="contact-div">
+                <span class="test">    
+                    <span class="contact-p">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris,
+                            congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus.
+                            Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus.
+                            Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.
+                        </p>
+                    </span>
+                
+                    <span class="contact-a">
+                        <a href="#" class="about__button-primary">Contattaci</a>
+                    </span>
+                </span>
+
+                <span class="contact-img">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_online_information_4ui6.png">
+                </span>
+            </div>
+        </section>
