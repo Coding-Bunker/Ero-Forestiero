59ece6ae94062d6ebd447cc14382f18c14aeddf8
andrea-artuso <ultrandr3x@gmail.com>
2021-01-20 13:05:05 +0100

    🎨 STYLE: mobile responsive added to corsi section

--- front-page.php
+++ front-page.php
@@ -1,41 +1,5 @@
 <?php get_header(); ?>
 
-        <!-- Inizio sezione intro - Author: Andrea -->
-        <section class="intro clearfix">
-            <span class="intro__left-column">
-            <h1 class="intro__title">Imparare l'italiano si può ed è anche <br><strong>gratis</strong>!</h1>
-
-            <div class="intro-links">
-                <a href="#" class="intro__button-primary"><strong>Inizia ad imparare</strong></a>
-                <a href="#" class="intro__link">Scropri l'offerta formativa</a>
-            </div>
-            </span>
-
-            <span class="intro__right-column">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ero-forestiero-logo.png" alt="ero-forestiero-logo" class="intro__image">
-            </span>
-        </section>
-        <!-- Fine sezione intro -->
-
-        <!-- Inizio sezione Chi Siamo - Author: Andrea -->
-        <section class="who-we-are clearfix">
-            <h2 class="about__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-
-            <div class="about__columns-div">
-                <span class="about__column">
-                    <p class="about__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris, congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus. Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus. Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.</p> <br><br>
-                    <p class="about__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris, congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus. Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus. Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.</p>
-                </span>
-                
-                <span class="about__column-image">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_teacher_35j2 1.png" alt="about-teacher-illustration">
-                </span>
-            </div>
-
-            <a href="#" class="about__button-primary">Scopri di più</a>
-        </section>
-        <!-- Fine sezione Chi Siamo -->
-
         <!-- Inizio sezione Corsi - Author: Andrea -->
         <section class="courses clearfix">
             <h2 class="courses__title">I nostri corsi</h2>
