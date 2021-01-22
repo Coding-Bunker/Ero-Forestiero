2f6888c09bfd1fcc696f76435afb48b606eae7d3
andrea-artuso <ultrandr3x@gmail.com>
2021-01-22 15:38:25 +0100

    🎨 STYLE: added responsive version of 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+    <!-- Pagina 404 - Author: Andrea -->
     <div class="error-page__image-container">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_page_not_found_su7k 1.png" alt="error-page image">
         <div class="error-page__text-container">
