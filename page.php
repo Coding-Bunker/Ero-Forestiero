f7f4aef01587d2c034517c68f5d1ba49ff5d9615
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-02-15 16:18:34 +0100

    Intro section chi siamo page

--- page.php
+++ page.php
@@ -1,3 +1,11 @@
-<?php
+<?php get_header(); ?>
 
-?>
+<div class="page-container">
+    <?php
+    if (is_page('chi-siamo')) {
+        get_template_part('template-parts/page', 'chisiamo');
+    }
+    ?>
+</div>
+
+<?php get_footer(); ?>
---
+++ template-parts/page-chisiamo.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/chi-siamo.css" />
+
+<div class="intro-section">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Set ludovico lacus, varius
+        non ipsum vel, pharetra luctus tellus
+    </p>
+
+    <div class="intro-paragraph">
+        <p>
+            Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed
+            nisi lacus, varius non ipsum vel,
+            pharetra luctus tellus.
+        </p>
+
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Sed nisi lacus, varius non ipsum
+                vel, pharetra luctus tellus.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Sed nisi lacus, varius non ipsum
+                vel, pharetra luctus tellus.
+            </p>
+        </div>
+    </div>
+
+    <div class="comune">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/comune-di-vigevano.png">
+        <div>
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi lacus, varius non ipsum vel, pharetra luctus tellus"
+            </p>
+            <p>
+                - Comune di Vigevano
+            </p>
+        </div>
+    </div>
+</div>
