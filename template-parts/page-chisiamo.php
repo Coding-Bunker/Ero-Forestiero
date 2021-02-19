c09c7862df498a3b7247a107a3bba9a8a1136783
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-02-19 18:34:28 +0100

    Added our history section

--- template-parts/page-chisiamo.php
+++ template-parts/page-chisiamo.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/chi-siamo.css" />
 
 <div class="intro-section">
     <p>
@@ -7,7 +6,7 @@
         non ipsum vel, pharetra luctus tellus
     </p>
 
-    <div class="intro-paragraph">
+    <section class="intro-paragraph">
         <p>
             Lorem ipsum dolor sit amet,
             consectetur adipiscing elit. Sed
@@ -28,9 +27,9 @@
                 vel, pharetra luctus tellus.
             </p>
         </div>
-    </div>
+    </section>
 
-    <div class="comune">
+    <section class="comune">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/comune-di-vigevano.png">
         <div>
             <p>
@@ -40,5 +39,31 @@
                 - Comune di Vigevano
             </p>
         </div>
-    </div>
+    </section>
+
+    <section class="storia">
+        <h2>La nostra storia</h2>
+
+        <div class="storia-div">
+            <span class="storia-paragraph">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris,
+                    congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus.
+                    Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus.
+                    Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris, congue non
+                    porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus. Integer et lacinia tortor.
+                    Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus. Pellentesque augue orci,
+                    faucibus non augue quis, rhoncus ornare nulla.
+                </p>
+            </span>
+
+            <span class="storia-img">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_environment_iaus 1.png">
+            </span>
+        </div>
+    </section>
 </div>
