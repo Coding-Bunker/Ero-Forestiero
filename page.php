0badc14c009aad6e1d0b0e2c7ac1f5b0f85eb65b
andrea-artuso <ultrandr3x@gmail.com>
2021-02-20 10:08:48 +0100

    🎉 FEATURE: added maintenance page

--- page.php
+++ page.php
@@ -1,3 +1,8 @@
-<?php
+<div class="page-container">
 
-?>
+    <?php
+        if (is_page('maintenance')) {
+            get_template_part('template-parts/page', 'maintenance');
+        }
+    ?>
+</div>
---
+++ template-parts/page-maintenace.php
@@ -0,0 +1,9 @@
+
+    <!-- Pagina Maintenance - Author: Andrea -->
+    <div class="maintenance-page__image-container">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_QA_engineers_dg5p 1.png" alt="maintenance-page image">
+        <div class="maintenance-page__text-container">
+            <h2>Ci dispiace, siamo in manutenzion</h2>
+            <h3>Torneremo a breve</h3>
+        </div>
+    </div>
