ccd435ad5d48677e64c9b6f72786f194068b3126
andrea-artuso <ultrandr3x@gmail.com>
2021-02-21 10:42:24 +0100

    🎉 FEATURE: added offerta-formativa page with intro and courses section

--- page.php
+++ page.php
@@ -1,3 +1,11 @@
-<?php
+<?php get_header(); ?>
 
-?>
+<div class="page-container">
+    <?php
+    if (is_page('offerta-formativa')) {
+        get_template_part('template-parts/page', 'offertaformativa');
+    }
+    ?>
+</div>
+
+<?php get_footer(); ?>
---
+++ template-parts/page-offertaformativa.php
@@ -0,0 +1,79 @@
+
+<section class="offerta-formativa__intro">
+    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+
+    <h2 class="courses__title offerta-formativa__courses-title">I nostri corsi</h2>
+
+<div class="courses__card-container">
+    <div class="courses__card">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons8-seed-96 1.png" alt="seed-icon" class="courses__card-image">
+        <h3 class="courses__card-title">Base</h3>
+        <p class="courses__card-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+
+    <div class="courses__card">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons8-garden-96 1.png" alt="seed-icon" class="courses__card-image">
+        <h3 class="courses__card-title">Intermedio</h3>
+        <p class="courses__card-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+
+    <div class="courses__card">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons8-tree-planting-96 1.png" alt="seed-icon" class="courses__card-image">
+        <h3 class="courses__card-title">Avanzato</h3>
+        <p class="courses__card-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+</div>
+</section>
+
+<div class="offerta-formativa__testimony">
+    <h3 class="testimony__text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit"</h3>
+    <br>
+    <h3 class="testimony__author">Antonio Verdi</h3>
+</div>
+
+<section class="offerta-formativa__courses">
+    <h2 class="courses__title offerta-formativa__courses-title">Tutti i nostri corsi comprendono</h2>
+
+    <div class="offerta-formativa__courses-container">
+        <div class="courses-main-image">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_knowledge_g5gf 1.png" alt="woman-looking-website">
+        </div>
+
+        <div class="courses-vertical-cards">
+
+            <div class="vertical-card">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/tick-circle.png" alt="green-tick-circle">
+                <div class="vertical-card-text">
+                    <h5>Lorem ipsum</h5>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+                </div>
+            </div>
+
+            <div class="vertical-card">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/tick-circle.png" alt="green-tick-circle">
+                <div class="vertical-card-text">
+                    <h5>Lorem ipsum</h5>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+                </div>
+            </div>
+
+            <div class="vertical-card">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/tick-circle.png" alt="green-tick-circle">
+                <div class="vertical-card-text">
+                    <h5>Lorem ipsum</h5>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+                </div>
+            </div>
+
+            <div class="vertical-card">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/tick-circle.png" alt="green-tick-circle">
+                <div class="vertical-card-text">
+                    <h5>Lorem ipsum</h5>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
