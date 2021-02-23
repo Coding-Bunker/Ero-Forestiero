0c741b446074fcec621296011467ded149de1b36
ImAl3x03 <alessandrodimaria3@gmail.com>
2021-02-23 21:49:42 +0100

    Added become a volunteer section

--- template-parts/page-chisiamo.php
+++ template-parts/page-chisiamo.php
@@ -397,4 +397,32 @@
             <a href="#">Conosci altri studenti</a>
         </div>
     </section>
+
+    <section class="volunteer">
+    <h2><strong>Diventa un volontario</strong></h2>
+
+    <div class="volunteer-div" style="">
+        <span class="volunteer-img">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_quiz_nlyh 1.png">
+        </span>
+
+        <span class="volunteer-p">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa consectetur,
+                architecto expedita temporibus illo cum nihil veniam laborum, quasi iusto inventore sunt,
+                autem et numquam placeat suscipit odit consequatur velit!
+            </p>
+
+            <br>
+
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus,
+                possimus in aliquam natus eveniet unde dolore magni sequi alias dignissimos
+                repellendus ducimus adipisci beatae neque consequatur voluptatibus consequuntur labore.
+            </p>
+        </span>
+    </div>
+
+    <a href="#" class="about__button-primary">Diventa un volontario</a>
+</section>
 </div>
