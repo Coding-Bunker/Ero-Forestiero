02ad2b98dfb5de78895d77efe73ae9d1b82b9bb7
andrea-artuso <ultrandr3x@gmail.com>
2021-02-23 18:30:16 +0100

    🎨 STYLE: added tablet and mobile responsive

--- template-parts/page-maintenance.php
+++ template-parts/page-maintenance.php
@@ -1,9 +1,11 @@
-
+    <head>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/page-maintenance.css" type="text/css" media="all" />
+    </head>
     <!-- Pagina Maintenance - Author: Andrea -->
     <div class="maintenance-page__image-container">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_QA_engineers_dg5p 1.png" alt="maintenance-page image">
         <div class="maintenance-page__text-container">
-            <h2>Ci dispiace, siamo in manutenzion</h2>
+            <h2>Ci dispiace, siamo in manutenzione</h2>
             <h3>Torneremo a breve</h3>
         </div>
     </div>
