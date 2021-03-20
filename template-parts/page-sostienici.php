3968f371a8781a08742c64ec52728050820bd55d
andrea-artuso <ultrandr3x@gmail.com>
2021-03-20 13:05:33 +0100

    🎉 FEATURE: added 730 model section with responsive

--- template-parts/page-sostienici.php
+++ template-parts/page-sostienici.php
@@ -24,7 +24,7 @@
         </span>
 
         <span class="about__column-image left-image">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_send_gift_7o4m 1.png" alt="send-gift-illustration">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/mission-illustration.png" alt="send-gift-illustration">
         </span>
     </div>
 </section>
@@ -42,5 +42,26 @@
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/paypal-logo.png" alt="paypal-logo">
         </div>
 
+    </div>
+</section>
+
+<section class="right-image-section">
+    <h1 class="model730-title about__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit. (730)</h1>
+
+    <div class="about__columns-div">
+        <span class="about__column">
+            <h2 class="image-section_title">Modello 730</h2>
+            <p class="about__paragraph support__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris, congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus. Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus. Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.</p> <br><br>
+            <p class="about__paragraph support__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris, congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus. Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus. Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.</p>
+
+            <button class="btn btn-outline-success volunteer-btn download-button" type="submit">
+                <a href="">Scarica il Modello 730</a>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/download-icon.png" alt="download icon">
+            </button>
+        </span>
+
+        <span class="about__column-image">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/undraw_attached_file_n4wm 1.png" alt="model730-illustration">
+        </span>
     </div>
 </section>
