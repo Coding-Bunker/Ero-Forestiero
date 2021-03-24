23b5c42074a73281e0c0394c9630a71e08e6a3fe
andrea-artuso <ultrandr3x@gmail.com>
2021-03-24 18:35:06 +0100

    Added contatti-page template

--- page.php
+++ page.php
@@ -8,6 +8,9 @@
     if (is_page('chi-siamo')) {
         get_template_part('template-parts/page', 'chisiamo');
     }
+    if (is_page('contatti')) {
+        get_template_part('template-parts/page', 'contatti');
+    }
     ?>
 </div>
 
