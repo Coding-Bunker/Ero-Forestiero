16f7e124c7d3514c7c35c279ad549fb65ed6b71d
Pasquale Giorgio <pasqualegiorgio9868@gmail.com>
2021-07-11 10:43:24 +0200

    Add some real world text to the page Chi siamo

--- template-parts/page-chisiamo.php
+++ template-parts/page-chisiamo.php
@@ -1,8 +1,6 @@
 <div class="intro-section">
     <p>
-        Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Set ludovico lacus, varius
-        non ipsum vel, pharetra luctus tellus
+        Ero Forestiero è prima di tutto le persone che la compongono: Volontari e Studenti.
     </p>
 
     <section class="intro-paragraph">
@@ -46,19 +44,18 @@
         <div class="storia-div">
             <span class="storia-paragraph">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris,
-                    congue non porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus.
-                    Integer et lacinia tortor. Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus.
-                    Pellentesque augue orci, faucibus non augue quis, rhoncus ornare nulla.
-                </p>
-
-                <br>
-                
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu interdum leo. Aenean diam mauris, congue non
-                    porta tincidunt, venenatis sed quam. Aliquam vestibulum eros sit amet fringilla finibus. Integer et lacinia tortor.
-                    Maecenas vitae velit ac sapien consequat fermentum. Nulla ut dui ut arcu auctor rhoncus. Pellentesque augue orci,
-                    faucibus non augue quis, rhoncus ornare nulla.
+                    L’Associazione ERO FORESTIERO affonda le sue radici, solidamente piantate ormai da anni, nel territorio di Vigevano attraverso l’operato inesauribile e costante dei propri volontari.<br>
+                    Nel lontano 1996, la fondatrice Agnese Magnani ebbe l’idea di sviluppare un progetto di aiuto concreto nei confronti di chi ha deciso di partire da lontano per venire a vivere in Italia.<br> 
+                    Agnese fondò questa Associazione in maniera conforme al suo carattere.<br>
+                    Era schietta, coraggiosa e solitaria e si rese subito conto che occuparsi di immigrazione era una necessità e un lavoro delicato, da portare avanti con un’idea equilibrata rispetto alle difficoltà ed alle soddisfazioni che questo poteva portare con sé. <br>
+                    Viva in lei l’urgenza di intervenire, fornendo dei servizi che mancavano sul territorio, nella consapevolezza che l’immigrazione non sia di per sé categorizzabile come un bene o un male assoluto ma che sia anche un fatto ineluttabile e che dunque ci si debba offrire come volontari affinché sia una risorsa per la società. <br>
+                    Pur essendo Ero Forestiero una sua creatura, aveva compreso che il suo impegno avrebbe prodotto frutti maturi solo se fosse riuscita ad infondere la sua stessa passione in volontari che avrebbero potuto portare l’Associazione a maturare anche quando lei se ne fosse andata. <br>
+                    Così è stato. Ero Forestiero vive oggi oltre Agnese ma con Agnese nel cuore, proprio come abbiamo voluto prometterle quando le forze iniziarono a mancarle. <br>
+                    Ero Forestiero offre, grazie a Lei, alfabetizzazione per stranieri con un taglio umano e dà la possibilità ai suoi Volontari di crescere al suo interno con lo stesso spirito. <br>
+                    Con questo stesso spirito portiamo avanti ancora oggi le nostre attività. <br>
+                    Era il 1996! <br>
+                    Se percorriamo a ritroso il filo della memoria ritroviamo una Vigevano differente da quella di oggi, come diverse erano le condizioni socio-economiche del nostro Paese e i problemi con cui si confrontava, come diversi eravamo noi. <br>
+                    Simile ad allora è però il modo in cui ci rapportiamo a chi è comunemente definito straniero ed è di questo che Ero Forestiero si occupa grazie all’opera dei suoi volontari.
                 </p>
             </span>
 
@@ -72,10 +69,8 @@
         <h2>I nostri Volontari</h2>
 
         <p>
-            I volontari sono il primo mattoncino alla base di questa Associazione. Infatti l’Associazione
-            opera grazie a tante persone con competenze ed esperienze di vita diverse, che scelgono di
-            dedicare un po’ del loro tempo libero ad altre persone e all’obiettivo comune di favorirne
-            l’integrazione e la socializzazione.
+            I volontari sono il primo mattoncino alla base di questa Associazione. 
+            Sono tante persone con competenze ed esperienze di vita diverse, che scelgono di dedicare un po’ del loro tempo libero agli stranieri con l’obiettivo comune di favorirne l’integrazione e la socializzazione.
         </p>
 
         <div class="card-flexbox">
@@ -236,14 +231,9 @@
         <h2>I nostri Studenti</h2>
 
         <p>
-            La parola studenti forse fa venire in mente uno scolaretto, tra i 7 e i 15 anni, che con il suo
-            zainetto si prepara ad andare a scuola un po' contro voglia. Bene, scordatevi questa
-            immagine
-        </p>
-
-        <p>
-            Gli studenti di Ero Forestiero sono il secondo mattoncino alla base dell'Associazione, senza i
-            quali la nostra attività non avrebbe senso di esistere.
+            Gli studenti di Ero Forestiero sono il secondo mattoncino alla base dell’Associazione, senza i quali la nostra attività non avrebbe senso di esistere.
+            In genere sono uomini e donne, fra i 18 e 65 anni di età: disoccupati, casalinghe, studenti, lavoratori. 
+            Provengono tutti da paesi diversi e hanno conoscenze diverse e storie di vita diverse. 
         </p>
 
         <div class="card-flexbox">
